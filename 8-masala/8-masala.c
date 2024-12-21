6a04c013f0ecf6637e454c0260894df74f33a07f
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-21 14:32:07 +0500

    ASCCi tabledagi sonlarni harfga o'zgartirib beradigan dastur

--- 8-masala/8-masala.c
+++ 8-masala/8-masala.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 int main(){
-    FILE *input = fopen("input.txt","r");
+    FILE *input = fopen("input.txt","r");   // fayllar ochildi 
     FILE *output = fopen("output.txt","w");
 
-    if (input  == NULL){
-        printf("Faylni o'qib bo'lmadi!");
-        return 1;
+    if (input  == NULL){        // Agar fayl NULL ga teng bo'lsa 
+        printf("Faylni o'qib bo 'lmadi!");
+        return 1;                         // Dastur o'chadi
     }
     int asci_num;
-
-    while (fscanf(input,"%d",&asci_num) == 1){
-         fprintf(output,"%c",(char)asci_num);
+ 
+    while (fscanf(input,"%d",&asci_num) == 1){ 
+         fprintf(output,"%c",(char)asci_num);     // Charga o'tgazamiz 
     }
     fclose(input);
     fclose(output);
